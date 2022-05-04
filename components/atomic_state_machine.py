f55b358114307b28f59117d2e02c20604cb27bfc
Melet Chirino <melet.chirino@gmail.com>
2022-05-04 16:55:42 +0200

    [UPDATE] Atomic state machine

--- components/atomic_state_machine.py
+++ components/atomic_state_machine.py
@@ -22,18 +22,44 @@ class AtomicStateMachine:
         self.transition_list = []
         # actual_state, event, next_state
         self.state_names = []
+        self.transtion_list = []
+        self.state_function_list = []
         if kwargs.get('state_names'):
             self.state_names = kwargs['state_names']
 
     def state_names(self, *state_names):
         self.state_names = state_names
 
+    def set_transition_list(self, *transition_list):
+        # transition format (init_state, next_state, [function], event)
+        self.transition_list = []
+        for transition in transition_list:
+            if len(transition) != 4:
+                raise Exception("Unvalid Transition Format")
+            else:
+                self.append(transition)
+
     def reset(self):
         self.state = 0
 
     def transition(self, event):
-        pass
+        for transition in self.transition_list:
+            if self.state == transition[0]:
+                if event == transition[3]:
+                    self.state = transition[1]
+                    for func in transition[2]:
+                        func()
+                    return 0
 
     def attach_events(self, *events):
         for event in events:
             event.attach(self)
+
+    def set_state_function_list(self, *state_function_list):
+        # Functions should be written in same state order, it MUST be a list of functions, if no functions place empty list
+        for state_function in state_function_list:
+            self.state_function_list.append(state_function)
+    def state_function(self):
+        funcs = self.state_function_list[self.state]
+        for func in funcs:
+            fun()
